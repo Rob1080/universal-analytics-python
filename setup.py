5f26a8bac6894d459803d71e8c034bfa9e13627f
Sam Briesemeister <sam@analyticspros.com>
2013-11-09 22:27:47 -0800

    Updated setup + package description for release

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 try:
-    long_description=open('README.md', 'rt').read()
+    long_description=open('DESCRIPTION.rst', 'rt').read()
 except Exception:
-    long_description=""
+    long_description="Universal Analytics in Python; an implementation of Google's Universal Analytics Measurement Protocol"
 
 setup(
     name = "universal-analytics-python",
@@ -11,7 +11,12 @@ setup(
     long_description = long_description,
 
     version = "0.1",
+
     author = 'Sam Briesemeister',
+    author_email = 'opensource@analyticspros.com',
+
+    url = 'https://github.com/analytics-pros/universal-analytics-python',
+
     license = 'BSD',
     packages = ["UniversalAnalytics"],
 
